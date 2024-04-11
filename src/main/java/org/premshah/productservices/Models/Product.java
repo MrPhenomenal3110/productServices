c44679ed7c8fba97918f6cc667473c8ef6a2648e
Prem B. Shah <techie.prembshah@gmail.com>
2024-04-11 17:52:44 +0530

    Added Exceptions and Repositories (Database is not yet set-up)

--- src/main/java/org/premshah/productservices/Models/Product.java
+++ src/main/java/org/premshah/productservices/Models/Product.java
@@ -1,15 +1,21 @@
 package org.premshah.productservices.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String name;
     String description;
     double price;
     String image;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     Category category;
 }
